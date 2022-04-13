77856612378808d091b0b5a4b4bf0de221c309bf
Ryan <ryanpburnett@yahoo.com>
2022-04-13 16:43:01 -0500

    cleaned up some old code

--- assets/script.js
+++ assets/script.js
@@ -2,17 +2,16 @@
 
 var choices = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes", "Most likely", "Outlook good.", "Yes.", "Signs point to Yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful"]
 
-document.getElementById("button").addEventListener("click", eightBall)
+document.getElementById("button").addEventListener("click", eightBall);
 
 // CSS of magic 8 ball, shake animation
 // fix opacity- only fades in on frist click
 function eightBall() {
+    var pickChoice = choices[Math.floor(Math.random() * choices.length)];
+    var result = document.getElementById("result");
 
-    var pickChoice = choices[Math.floor(Math.random() * choices.length)]
-    var result = document.getElementById("result")
-
-    result.innerHTML = ""
-    result.classList.add('fade-in')
+    result.innerHTML = "";
+    result.classList.add('fade-in');
     result.innerHTML = pickChoice;
     console.log(pickChoice);
 }
